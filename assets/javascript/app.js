e632f7e59e1b0348eebfcbc41c155b593bc1cfaf
Christopher Miller <christopher@transcriptic.com>
2017-06-15 19:13:50 -0700

    added initial firebase database config and script link

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD1UCXTw5xWE5WehTfqh0AK-K2asMyf4S4",
+    authDomain: "couplette-b67ce.firebaseapp.com",
+    databaseURL: "https://couplette-b67ce.firebaseio.com",
+    projectId: "couplette-b67ce",
+    storageBucket: "couplette-b67ce.appspot.com",
+    messagingSenderId: "934836634497"
+  };
+  firebase.initializeApp(config);
